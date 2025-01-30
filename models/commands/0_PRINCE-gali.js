95ee2598dc489a3b5d8f6e920942215f83fc1330
Ayansh Rajput <alexdubey050@gmail.com>
2025-01-30 14:30:51 +0530

    Update 0_PRINCE-gali.js

--- models/commands/0_PRINCE-gali.js
+++ models/commands/0_PRINCE-gali.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("fuck")==0 || event.body.indexOf("mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("bal")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("maa ki chut")==0 || event.body.indexOf("xod")==0 || event.body.indexOf("behen chod")==0 || event.body.indexOf("🖕")==0 || event.body.indexOf("madarchod")==0 || event.body.indexOf("Bsdke")==0 || event.body.indexOf("gandu")==0) {
 		var msg = {
-				body: "(Boss Dk , Gali q Dete Ho. Lund Katke Hath M rakh Dungi)",
+				body: "(Boss Dk , Gali q Dete Ho. Choomu si sakal kee)",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
