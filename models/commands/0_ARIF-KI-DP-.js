c0f3c31b147303908b783b7b18e785f7f3fd739d
Ayansh Rajput <alexdubey050@gmail.com>
2025-01-31 12:50:01 +0530

    Update 0_ARIF-KI-DP-.js

--- models/commands/0_ARIF-KI-DP-.js
+++ models/commands/0_ARIF-KI-DP-.js
@@ -15,8 +15,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   let react = event.body.toLowerCase();
   if(react.includes("ayansh") ||
      react.includes("ayansh") || react.includes("ayansh") || react.includes("@𓏵𓋠𒅓𓏪𓉚𝐀𝐘𝐀𝐍𝐒𝐇𓉛𓏪𒅓𓋠𓏵 𒆜𒋟❮𝐑𝐀𝐉𝐀𝐒𝐓𝐀𝐍𒀭𝗞𝗔𒀭𝗕𝗔𝗗𝗦𝗛𝗔𝗛❯𒋟𒆜 𒀭𒀮𓁫𓂔𓏵𓏜𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞𓏜𓏵𓂔𓁬𒀮𒀭 𓃉𓃊𓃑𓄋𝐈𝐓𝐒𓇻︎𓇻𝐁𝐑𝐀𝐍𝐃𓄋𓃑𓃊𓃉 𓆈 y -  【‿】") ||
-react.includes("ayansh babu") ||
-react.includes("ayansh boss")) {
+react.includes("ayansh") ||
+react.includes("ayansh")) {
     var msg = {
         body: `𝐃𝐄𝐊𝐇𝐎 𝐌𝐄 𝐀 𝐆𝐀𝐘𝐀 𝐀𝐀𝐏𝐊𝐀 𝐁𝐎𝐒𝐒 𝐀𝐘𝐀𝐍𝐒𝐇 🙈 😎 𓆩♡𓆪`,attachment: fs.createReadStream(__dirname + `/ARIF-BABU/AYANSH.mp4`)
       }
