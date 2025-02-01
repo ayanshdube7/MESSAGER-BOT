35a092c6bb9b3c956402744827c1b8174a192f80
Ayansh Rajput <alexdubey050@gmail.com>
2025-02-01 16:36:23 +0530

    Update 0_ARIF-BURGER.js

--- models/commands/0_ARIF-BURGER.js
+++ models/commands/0_ARIF-BURGER.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("BURGER")==0 || event.body.indexOf("Burger")==0 || event.body.indexOf("burger")==0 || event.body.indexOf("Barger")==0) {
 		var msg = {
-				body: "💝 YE LO BBY BURGER KHAO 🍔\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..𝐏𝐫𝐢𝐧𝐜𝐞❤️-)",
+				body: "💝 YE LO BBY BURGER KHAO 🍔\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..Ayanshx❤️-)",
 				attachment: 
 fs.createReadStream(__dirname + `/ARIF-BABU/BURGER.jpg`)
 			}
