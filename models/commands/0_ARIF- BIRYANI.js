d972c4f34ab8b737c080cd91b984b43717371c14
Ayansh Rajput <alexdubey050@gmail.com>
2025-02-01 15:16:48 +0530

    Update 0_ARIF- BIRYANI.js

--- models/commands/0_ARIF- BIRYANI.js
+++ models/commands/0_ARIF- BIRYANI.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("BIRYANI")==0 || event.body.indexOf("Biryani")==0 || event.body.indexOf("biryani")==0 || event.body.indexOf(".biryani")==0) {
 		var msg = {
-				body: "💝 YE LO BBY BIRYANI KHAO 🥗\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..𝐏𝐫𝐢𝐧𝐜𝐞❤️-)",
+				body: "💝 YE LO BBY BIRYANI KHAO 🥗\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..Ayansx❤️-)",
 				attachment: 
 fs.createReadStream(__dirname + `/ARIF-BABU/BIRYANI.jpg`)
 			}
