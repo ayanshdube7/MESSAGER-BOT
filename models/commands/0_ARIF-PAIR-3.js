f9e3368adcb71f0044b8b2142b165d8944c426d4
Ayansh Rajput <alexdubey050@gmail.com>
2025-02-01 19:27:10 +0530

    Update 0_ARIF-PAIR-3.js

--- models/commands/0_ARIF-PAIR-3.js
+++ models/commands/0_ARIF-PAIR-3.js
@@ -1,9 +1,10 @@
 module.exports.config = {
-  name: "pair3",
+  name: "pair7",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "faiz Ansari",
-  description: "It's a compound :>",
+  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  description: "Get pair From Mention",
+  usePrefix: true,
   commandCategory: "Giải trí",
   usages: "[@mention]",
   dependencies: {
@@ -20,7 +21,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   let pathImg = __dirname + "/cache/background.png";
   let pathAvt1 = __dirname + "/cache/Avtmot.png";
   let pathAvt2 = __dirname + "/cache/Avthai.png";
-  
+
   var id1 = event.senderID;
   var name1 = await Users.getNameUser(id1);
   var ThreadInfo = await api.getThreadInfo(event.threadID);
@@ -55,14 +56,14 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   var cc = ["0", "-1", "99,99", "-99", "-100", "101", "0,01"];
   var rd2 = cc[Math.floor(Math.random() * cc.length)];
   var djtme = [`${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd2}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`];
-  
+
   var tile = djtme[Math.floor(Math.random() * djtme.length)];
 
   var background = [
-  "https://i.imgur.com/CYsOXjr.jpg",
+  "https://i.imgur.com/c5XwiAJ.jpg",
   ];
   var rd = background[Math.floor(Math.random() * background.length)];
-  
+
   let getAvtmot = (
     await axios.get(
       `https://graph.facebook.com/${id1}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
@@ -92,13 +93,14 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(baseAvt1, 230, 250, 240, 327);
-  ctx.drawImage(baseAvt2, 725, 245, 240, 330);
+  ctx.drawImage(baseAvt1, 65,
+20, 300, 370);
+  ctx.drawImage(baseAvt2, 430, 345, 252, 362);
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
   fs.removeSync(pathAvt1);
   fs.removeSync(pathAvt2);
-  return api.sendMessage({ body: `🍫${name1}🍫\nLOVE🍫${name2}🍫\n✧═══•❁『${tile}%』❁•═══✧`,
+  return api.sendMessage({ body: `⃣Ᏹ̳͌͌͌͌͌͌͌͌͌Ꮤ̳͌͌͌͌͌͌͌͌͌Ꭼ̳͌͌͌͌͌͌͌͌͌Ꮣ̳͌͌͌͌͌͌͌͌͌Ꮣ̳͌͌͌͌͌͌͌͌͌Ꮸ̳͌͌͌͌͌͌͌͌͌Ꮻ̳͌͌͌͌͌͌͌͌͌Ꮇ̳͌͌͌͌͌͌͌͌͌Ꭼ̳͌͌͌͌͌͌͌͌͌Ᏹ̳͌͌͌͌͌͌͌͌͌Ꮬ̳͌͌͌͌͌͌͌͌͌Ᏹ̳͌͌͌͌͌͌͌͌͌Ꮬ̳͌͌͌͌͌͌͌͌͌⃣\n`,
             mentions: [{
           tag: `${name2}`,
           id: id2
@@ -106,4 +108,4 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
       event.threadID,
       () => fs.unlinkSync(pathImg),
       event.messageID);
-}
+  }
