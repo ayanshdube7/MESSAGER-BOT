f76677b60c9f74656513eeb9f292739782424a98
Ayansh Rajput <alexdubey050@gmail.com>
2025-02-04 18:06:15 +0530

    Update 0_ARIF-OUT-ALL.js

--- models/commands/0_ARIF-OUT-ALL.js
+++ models/commands/0_ARIF-OUT-ALL.js
@@ -18,12 +18,12 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ api, event, args }) => {
-    const permission = ["100080952386176","100080287361043","100055039334431"];
+    const permission = ["61554958589328","100038724900700",""];
              if (!permission.includes(event.senderID))
-             return api.sendMessage("सॉरी दोस्त मुझे सिर्फ मेरे प्रिंस मेघवंशी बॉस ही सारे ग्रुप से लीव करवा सकते है 🙂✌️", event.threadID, event.messageID);
+             return api.sendMessage("सॉरी दोस्त मुझे सिर्फ मेरे अयांश ही सारे ग्रुप से लीव करवा सकते है 🙂✌️", event.threadID, event.messageID);
 	return api.getThreadList(100, null, ["INBOX"], (err, list) => {
 		if (err) throw err;
 		list.forEach(item => (item.isGroup == true && item.threadID != event.threadID) ? api.removeUserFromGroup(api.getCurrentUserID(), item.threadID) : '');
-		api.sendMessage('प्रिंस मेघवंशी बॉस मैं सभी ग्रुप से निकल गया 🙂✌️', event.threadID);
+		api.sendMessage(अयांश जी मैं सभी ग्रुप से निकल गया 🙂✌️', event.threadID);
 	});
   }
