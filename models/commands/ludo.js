e155fe465ec7cc8f0a4c9bbd2cff183e7ebf9809
Ayansh Rajput <alexdubey050@gmail.com>
2025-02-16 00:31:48 +0530

    Update ludo.js

--- models/commands/ludo.js
+++ models/commands/ludo.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = async function({ api, event, client, Users, __GLOBA
   var name = await Users.getNameUser(event.senderID);
   if (event.body.indexOf("Ludo ka link do bot")>=0 || event.body.indexOf("ludo khele")>=0 || event.body.indexOf("link do ludo ka")>=0 || event.body.indexOf("ludo me aao")>=0 || event.body.indexOf("Ludo me aao")>=0 || event.body.indexOf("Ludo Khele")>=0 || event.body.indexOf("LUDO KHELE")>=0 || event.body.indexOf("ludo join karo")>=0 || event.body.indexOf("ludo me aao jaldi")>=0 || event.body.indexOf("LUDO KA LINK DO")>=0 ) { 
     var msg = {
-        body: ` ${name} कोई काम धाम नही हैं किया अप के पास जब देखो लूडो लूडो खेलते रहते हो ।😈👈`
+        body: ` ${name} कोई काम धाम नही हैं किया अप के पास जब देखो लूडो लूडो खेलते रहते हो ।🤦🏻`
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🤐", event.messageID, (err) => {}, true)
