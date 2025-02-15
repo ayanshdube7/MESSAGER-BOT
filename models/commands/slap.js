92dc29d04bef6af6dcfcd6714c727009702536c1
Ayansh Rajput <alexdubey050@gmail.com>
2025-02-15 09:43:50 +0530

    Update slap.js

--- models/commands/slap.js
+++ models/commands/slap.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: "slap",
   version: "1.0.0",
-  hasPermssion: 0,
+  hasPermssion: 1,
   credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
   description: "Slap the friend tag",
   commandCategory: "general",
