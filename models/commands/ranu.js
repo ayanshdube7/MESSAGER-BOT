6f3bc8fd33fa6418d52d75101bf026b745a46749
Ayansh Rajput <alexdubey050@gmail.com>
2025-02-16 00:40:18 +0530

    Update ranu.js

--- models/commands/ranu.js
+++ models/commands/ranu.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("@pimdi")==0 || event.body.indexOf("@Sharif")==0 || event.body.indexOf("@pindi")==0 || event.body.indexOf("@Sharif Ladka")==0) {
     var msg = {
-        body: "ye rha pimdi🥹👇🏻",
+        body: "ye rha pimdi 🥹🥹🥹 👇🏻",
         attachment: fs.createReadStream(__dirname + `/cache/ranu.jpeg`)
       }
       api.sendMessage(msg, threadID, messageID);
