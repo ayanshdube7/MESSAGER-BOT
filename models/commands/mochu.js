181ee13f566755b89d331c06637618290144e0ee
Ayansh Rajput <alexdubey050@gmail.com>
2025-02-16 00:38:59 +0530

    Update mochu.js

--- models/commands/mochu.js
+++ models/commands/mochu.js
@@ -14,11 +14,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("@Rohit")==0 || event.body.indexOf("@kalu😜")==0 || event.body.indexOf("@Rohit Sharma")==0 || event.body.indexOf("@Rohit")==0) {
     var msg = {
-        body: "👇🏻yaha hai niche dekho👇🏻",
+        body: " Ye Raha Kalu 👇🏻",
         attachment: fs.createReadStream(__dirname + `/cache/mochu.jpeg`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("😁", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🌚", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
