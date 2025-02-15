ff1f34308f1097283f90d2e52c3427fb62a40552
Ayansh Rajput <alexdubey050@gmail.com>
2025-02-15 23:05:25 +0530

    Update and rename nam-photo.js to gdha-gdhi.js

--- models/commands/gdha-gdhi.js
+++ models/commands/gdha-gdhi.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "nam photo",
+  name: "gdha gdhy",
     version: "1.0.1",
   hasPermssion: 0,
   credits: "FAIZ ANSARI", 
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("chivam")==0 || event.body.indexOf("@Sharif Ladka")==0 || event.body.indexOf("@Yumiko Thakur")==0 || event.body.indexOf("@Rakesh Nishad")==0) {
+  if (event.body.indexOf("chivam")==0 || event.body.indexOf("@Dwivedi Shivam")==0 || event.body.indexOf("@Alex Rajput")==0 || event.body.indexOf("@Abhi")==0) {
     var msg = {
         body: "👇🏻yaha hai niche dekho👇🏻",
         attachment: fs.createReadStream(__dirname + `/cache/3334.jpg`)
