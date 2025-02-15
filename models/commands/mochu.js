b52bcbcdb2b5fffaceebdd7913496e522287dccd
Ayansh Rajput <alexdubey050@gmail.com>
2025-02-15 23:28:43 +0530

    Update commands

---
+++ models/commands/mochu.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "nam photo",
+    version: "1.0.1",
+  hasPermssion: 0,
+  credits: "FAIZ ANSARI", 
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "nam",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("")==0 || event.body.indexOf("@kalu😜")==0 || event.body.indexOf("@Rohit Sharma")==0 || event.body.indexOf("@Rohit")==0) {
+    var msg = {
+        body: "👇🏻yaha hai niche dekho👇🏻",
+        attachment: fs.createReadStream(__dirname + `/cache/mochu.jpeg`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😁", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+        }
